a0924221eb2b3fddf2e9bbc6beb2c7b20edb499e
Dmytro Holub <heap3d@gmail.com>
2024-07-23 15:39:59 +0300

    refactored: item_replicate item_align

--- scripts/replace_items_tools.py
+++ scripts/replace_items_tools.py
@@ -35,6 +35,7 @@ class Constraints:
 def get_size(item):
     if item.type != 'mesh' and item.type != 'meshInst':
         return [0.0, 0.0, 0.0]
+
     s_x, s_y, s_z = get_item_scale(item)
     if item.type == 'meshInst':
         corners = get_source_of_instance(item).geometry.boundingBox  # type: ignore
@@ -80,6 +81,7 @@ def get_tmp_folder(name):
         tmp_folder.channel('visible').set('allOff')  # type: ignore
         tmp_folder.select(replace=True)
         lx.eval('item.editorColor magenta')
+
     return tmp_folder
 
 
@@ -88,14 +90,14 @@ def get_item_scale(item):
     x = lx.eval('transform.channel scl.X ?')
     y = lx.eval('transform.channel scl.Y ?')
     z = lx.eval('transform.channel scl.Z ?')
+
     return [x, y, z]
 
 
-def set_item_scale(item, scale):
-    item.select(replace=True)
-    lx.eval('transform.channel scl.X {}'.format(scale[0]))
-    lx.eval('transform.channel scl.Y {}'.format(scale[1]))
-    lx.eval('transform.channel scl.Z {}'.format(scale[2]))
+def set_item_scale(item: modo.Item, scale: list[float]):
+    lx.eval(f'transform.channel scl.X {scale[0]} item:{item.id}')
+    lx.eval(f'transform.channel scl.Y {scale[1]} item:{item.id}')
+    lx.eval(f'transform.channel scl.Z {scale[2]} item:{item.id}')
 
 
 def get_replicator_source(item: modo.Item) -> modo.Item:
@@ -119,13 +121,13 @@ def make_replicator(prototype: modo.Item, point_source: modo.Item) -> modo.Item:
     replicator.select(replace=True)
     lx.eval(f'replicator.particle {point_source.id}')
     lx.eval(f'replicator.source {prototype.id}')
+
     return replicator
 
 
 def get_source_size(source: modo.Item) -> list[float]:
-    source_base = get_source_of_instance(source)
     source_scl_x, source_scl_y, source_scl_z = get_item_scale(source)
-    base_scl_x, base_scl_y, base_scl_z = get_size(source_base)
+    base_scl_x, base_scl_y, base_scl_z = get_size(get_source_of_instance(source))
     source_size = [base_scl_x * source_scl_x, base_scl_y * source_scl_y, base_scl_z * source_scl_z]  # type: ignore
 
     return source_size
@@ -136,7 +138,14 @@ def match_pos_rot(item: modo.Item, itemTo: modo.Item):
     lx.eval(f'item.match item rot average:false item:{item.id} itemTo:{itemTo.id}')
 
 
-def get_ratios(source_size: list[float], target_size: list[float], constraints: Constraints) -> list[float]:
+def match_scl(item: modo.Item, itemTo: modo.Item):
+    lx.eval(f'item.match item scl average:false item:{item.id} itemTo:{itemTo.id}')
+
+
+def get_ratios(source: modo.Item, target: modo.Item, constraints: Constraints) -> list[float]:
+    source_size = get_source_size(source)
+    target_size = get_size(target)
+
     # check if there are any 0.0 in source_size or target_size
     if any([f == 0.0 for f in (source_size + target_size)]):
         ratio_x = 1
@@ -177,138 +186,42 @@ def get_ratios(source_size: list[float], target_size: list[float], constraints: 
     return ratio_x, ratio_y, ratio_z  # type: ignore
 
 
-def item_replicate(source: modo.Item, target: modo.Item, constraints: Constraints):
-    source_size = get_source_size(source)
-    target_size = get_size(target)
+def set_visible(item: modo.Item, mode: str = 'default'):
+    lx.eval(f'item.channel locator$visible {mode} item:{item.id}')
 
+
+def item_replicate(source: modo.Item, target: modo.Item, constraints: Constraints):
     source_replicator = get_replicator_source(source)
     point_source = get_vertex_zero()
+
     source_align = make_replicator(source_replicator, point_source)
     source_align.setParent()
-
     match_pos_rot(source_align, target)
-
-    # initiate visibility on instance
-    source_align.select(replace=True)
-    lx.eval('item.channel locator$visible default')
-    # check if there are any 0.0 in source_size or target_size
-    if any([f == 0.0 for f in (source_size + target_size)]):
-        ratio_x = 1
-        ratio_y = 1
-        ratio_z = 1
-    else:
-        ratio_x = target_size[0] / source_size[0]
-        ratio_y = target_size[1] / source_size[1]
-        ratio_z = target_size[2] / source_size[2]
-
-    # determine which constraints are on
-    lock_mode = constraints.mode
-    lock_order = constraints.order
-    if lock_mode == 'XZ':
-        if lock_order == 'X':
-            ratio_z = ratio_x
-        else:
-            ratio_x = ratio_z
-    elif lock_mode == 'XYZ':
-        if lock_order == 'X':
-            ratio_y = ratio_x
-            ratio_z = ratio_x
-        elif lock_order == 'Y':
-            ratio_x = ratio_y
-            ratio_z = ratio_y
-        else:
-            ratio_x = ratio_z
-            ratio_y = ratio_z
-
-    # reset scale for disabled axis
-    if not constraints.use_x:
-        ratio_x = 1.0
-    if not constraints.use_y:
-        ratio_y = 1.0
-    if not constraints.use_z:
-        ratio_z = 1.0
-
-    # set scale
-    set_scale_factor(source_align, [ratio_x, ratio_y, ratio_z])
+    set_scale_factor(source_align, get_ratios(source, target, constraints))
 
     replace_item(item_to_insert=source_align,
                  item_to_remove=target,
                  item_to_remove_new_parent=get_tmp_folder(h3dc.TMP_FOLDER_NAME))
 
+    set_visible(source_align)
+
 
 def item_align(source: modo.Item, target: modo.Item, do_instance: bool, constraints: Constraints):
-    source_base = get_source_of_instance(source)
-
-    # get source scale
-    sx, sy, sz = get_item_scale(source)
-    # base size
-    bx, by, bz = get_size(source_base)
-    source_size = [bx * sx, by * sy, bz * sz]  # type: ignore
-    # get target size
-    target_size = get_size(target)
-
     if do_instance:
-        source_item = modo.Scene().duplicateItem(item=source_base, instance=True)
+        source_item = modo.Scene().duplicateItem(item=get_source_of_instance(source), instance=True)
     else:
         source_item = source
     source_item.setParent()  # type: ignore
-
-    modo.Scene().deselect()
-    source_item.select()  # type: ignore
-    target.select()
-    lx.eval('item.match item pos average:false item:{} itemTo:{}'.format(source_item.id, target.id))  # type: ignore
-    lx.eval('item.match item rot average:false item:{} itemTo:{}'.format(source_item.id, target.id))  # type: ignore
+    match_pos_rot(source_item, target)  # type: ignore
     if do_instance:
-        # initiate scale on instance
-        source_item.select(replace=True)  # type: ignore
-        source_base.select()  # type: ignore
-        lx.eval('item.match item scl')
-    # initiate visibility on instance
-    source_item.select(replace=True)  # type: ignore
-    lx.eval('item.channel locator$visible default')
-    # check if there are any 0.0 in source_size or target_size
-    if any([f == 0.0 for f in (source_size + target_size)]):
-        ratio_x = 1
-        ratio_y = 1
-        ratio_z = 1
-    else:
-        ratio_x = target_size[0] / source_size[0]
-        ratio_y = target_size[1] / source_size[1]
-        ratio_z = target_size[2] / source_size[2]
-
-    # determine which constraints are on
-    lock_mode = constraints.mode
-    lock_order = constraints.order
-    if lock_mode == 'XZ':
-        if lock_order == 'X':
-            ratio_z = ratio_x
-        else:
-            ratio_x = ratio_z
-    elif lock_mode == 'XYZ':
-        if lock_order == 'X':
-            ratio_y = ratio_x
-            ratio_z = ratio_x
-        elif lock_order == 'Y':
-            ratio_x = ratio_y
-            ratio_z = ratio_y
-        else:
-            ratio_x = ratio_z
-            ratio_y = ratio_z
-
-    # reset scale for disabled axis
-    if not constraints.use_x:
-        ratio_x = 1.0
-    if not constraints.use_y:
-        ratio_y = 1.0
-    if not constraints.use_z:
-        ratio_z = 1.0
-
-    # set scale
-    set_scale_factor(source_item, [ratio_x, ratio_y, ratio_z])
+        match_scl(source_item, get_source_of_instance(source))  # type: ignore
+    set_scale_factor(source_item, get_ratios(source, target, constraints))
 
     replace_item(item_to_insert=source_item,
                  item_to_remove=target,
                  item_to_remove_new_parent=get_tmp_folder(h3dc.TMP_FOLDER_NAME))
+
+    set_visible(source_item)  # type: ignore
 
 
 save_log = get_user_value(h3dc.USER_VAL_NAME_SAVE_LOG)
